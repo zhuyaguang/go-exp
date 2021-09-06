44082b8f4d802572beb3dcff06e067ecbc7a4074
朱亚光 <zhuyaguang1368@163.com>
2021-09-06 17:35:36 +0800

    webhook finished

--- k8s-practice/admission-registry/go.mod
+++ k8s-practice/admission-registry/go.mod
@@ -3,8 +3,8 @@ module admission-registry
 go 1.15
 
 require (
-	k8s.io/api v0.22.1
-	k8s.io/apimachinery v0.22.1
-	k8s.io/klog v1.0.0
+	k8s.io/api v0.20.4
+	k8s.io/apimachinery v0.20.4
 	k8s.io/client-go v0.20.4
+	k8s.io/klog v1.0.0
 )
--- k8s-practice/admission-registry/pkg/util.go
+++ k8s-practice/admission-registry/pkg/util.go
@@ -1,11 +1,13 @@
 package pkg
 
-
 import (
-"os"
+	"flag"
+	"os"
+	"path/filepath"
 
-"k8s.io/client-go/kubernetes"
-"k8s.io/client-go/rest"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd"
 )
 
 func WriteFile(filePath string, bts []byte) error {
@@ -23,13 +25,17 @@ func WriteFile(filePath string, bts []byte) error {
 }
 
 func InitKubernetesCli() (*kubernetes.Clientset, error) {
-	var (
-		err    error
-		config *rest.Config
-	)
-	if config, err = rest.InClusterConfig(); err != nil {
-		return nil, err
-	}
+	// InClusterConfig  panic: unable to load in-cluster configuration, KUBERNETES_SERVICE_HOST and KUBERNETES_SERVICE_PORT must be defined
+
+	// var (
+	// 	err    error
+	// 	config *rest.Config
+	// )
+	// if config, err = rest.InClusterConfig(); err != nil {
+	// 	return nil, err
+	// }
+	// 
+	config := getConfig()
 
 	// 创建ClientSet 对象
 	clientset, err := kubernetes.NewForConfig(config)
@@ -37,4 +43,34 @@ func InitKubernetesCli() (*kubernetes.Clientset, error) {
 		return nil, err
 	}
 	return clientset, nil
+}
+
+func getConfig() *rest.Config {
+	var err error
+	var config *rest.Config
+	var kubeconfig *string
+
+	if home := homeDir(); home != "" {
+		kubeconfig = flag.String("kubeconfig", filepath.Join(home, ".kube", "kind-config-my-cluster"), "(optional) absolute path to the kubeconfig file")
+	} else {
+		kubeconfig = flag.String("kubeconfig", "", "absolute path to the kubeconfig file")
+	}
+	flag.Parse()
+
+	// 使用 ServiceAccount 创建集群配置（InCluster模式）
+	if config, err = rest.InClusterConfig(); err != nil {
+		// 使用 KubeConfig 文件创建集群配置
+		if config, err = clientcmd.BuildConfigFromFlags("", *kubeconfig); err != nil {
+			panic(err.Error())
+		}
+	}
+
+	return config
+}
+
+func homeDir() string {
+	if h := os.Getenv("HOME"); h != "" {
+		return h
+	}
+	return os.Getenv("USERPROFILE") // windows
 }
